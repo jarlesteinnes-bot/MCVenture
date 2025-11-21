c3b5cbf4e6cb768d2c749c8feed71e7bb5f1e841
Jarle Steinnes <Jarlesteinnes@gmail.com>
2025-11-21 11:56:54 +0100

    Initial Commit

---
+++ MVVentures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVentures
+//
+//  Created by BNTF on 21/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVentures/MVVenturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVenturesApp.swift
+//  MVVentures
+//
+//  Created by BNTF on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVenturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
